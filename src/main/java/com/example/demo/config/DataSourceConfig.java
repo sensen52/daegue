d63ff7b8e9f0dd632869549d5f90cd7f32abdb89
sensen52 <125122104+sensen52@users.noreply.github.com>
2023-11-14 04:22:09 +0000

    Update DataSourceConfig.java
    trans password

--- src/main/java/com/example/demo/config/DataSourceConfig.java
+++ src/main/java/com/example/demo/config/DataSourceConfig.java
@@ -30,7 +30,7 @@ public class DataSourceConfig {
 		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
 		dataSource.setJdbcUrl("jdbc:mysql://15.164.139.112:3306/daeguyo");
 		dataSource.setUsername("root");
-		dataSource.setPassword("woals941215!");
+		dataSource.setPassword("Gytlstkfkd52^");
 
 		return dataSource;
 	}
